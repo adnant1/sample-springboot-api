dd7dad49e97ba9d7e66a76ef47e86703f3475edc
adnant1 <adnantayib8@gmail.com>
2025-05-08 17:40:15 -0400

    Initial commit

---
+++ src/main/java/com/sampleapi/demo/StudentConfig.java
@@ -0,0 +1,34 @@
+package com.sampleapi.demo;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student maria = new Student(
+                    "Adam",
+                    "adam12@gmail.com",
+                    LocalDate.of(2000, Month.JANUARY, 5),
+                    21
+            );
+            Student alex = new Student(
+                    "Alex",
+                    "alex12@gmail.com",
+                    LocalDate.of(2000, Month.JANUARY, 5),
+                    21
+            );
+
+            repository.saveAll(List.of(maria, alex));
+        };
+    }
+}
+    
